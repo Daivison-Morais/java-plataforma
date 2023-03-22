35e1fe5221cca1e0b5e000c90246e01f71e5a95c
Daivison-Morais <daivison13pinheiro@gmail.com>
2023-03-22 17:50:38 -0300

    criada a classe CursoDev

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+       CursoDev curso = new CursoDev();
+
+       curso.setBackEnd("banco de dados");
+       System.out.println(curso.getBackEnd());
+       
+    }
+}
---
+++ src/CursoDev.java
@@ -0,0 +1,17 @@
+public class CursoDev {
+    private String frontEnd;
+    private String backEnd;
+
+    public void setBackEnd(String backEnd) {
+      this.backEnd = backEnd;
+    }
+    
+    public String getBackEnd() {
+        return this.backEnd;
+    }
+
+    public String getFrontEnd(String frontEnd) {
+        return this.frontEnd;
+    }
+
+}
